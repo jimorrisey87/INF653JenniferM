3d6a58f69534ebe0866752c12f869a5eded896a7
Jennifer Morrisey <jimorrisey@mail.fhsu.edu>
2023-05-07 14:52:08 -0400

    changed port

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,7 +3,8 @@ const whitelist = [
     '',
    'https://dazzling-snickerdoodle-777101.netlify.app', 
     'http://127.0.0.1:5500', 
-    'http://localhost:3500'
+    'http://localhost:3500',
+    'http://localhost:3000'
 ];
 const corsOptions = {
     origin: (origin, callback) =>{
