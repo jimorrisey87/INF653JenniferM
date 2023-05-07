327ca0d47f6e5648391495b9dcf51a5509d6ef0f
Jennifer Morrisey <jimorrisey@mail.fhsu.edu>
2023-05-07 14:59:32 -0400

    kept node_modules

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,7 +2,7 @@
 const whitelist = [
     '',
    'https://dazzling-snickerdoodle-777101.netlify.app', 
-    'http://127.0.0.1:5500', 
+    'http://127.0.0.1:3500', 
     'http://localhost:3500',
     'http://localhost:3000'
 ];
